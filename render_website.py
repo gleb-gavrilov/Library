3bea2e8096d6c9c0bf36c86c33debf6e95648cc8
Gleb Gavrilov <karlord@mail.ru>
2020-05-18 21:54:40 +0300

    add two columns books render

--- render_website.py
+++ render_website.py
@@ -1,6 +1,8 @@
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from more_itertools import chunked
+import math
 
 
 def on_reload():
@@ -11,6 +13,9 @@ def on_reload():
     template = env.get_template('template.html')
     with open('library.json', encoding='utf-8') as file:
         library = json.load(file)
+    length = len(library) / 2
+    length = math.ceil(length)
+    library = chunked(library, length)
     rendered_page = template.render(library=library)
     with open('index.html', 'w', encoding='utf-8') as file:
         file.write(rendered_page)
